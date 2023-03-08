307dcd2e0b49f92384f8867f8d76e69907df250d
kyle Ezekiel Kuppusamy <kylekuppusamy@gmail.com>
2023-03-08 11:19:16 +0200

    -- Added button -- Added new activity -- Added Recycler view Learn how to use a Recycler view and display words to view

---
+++ app/src/main/java/com/example/wordguessinggame/DisplayWordsView.kt
@@ -0,0 +1,12 @@
+package com.example.wordguessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayWordsView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_words_view)
+
+    }
+}
