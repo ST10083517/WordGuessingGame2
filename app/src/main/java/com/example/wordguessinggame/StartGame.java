12edef4f3debcd39681fd8c4b31300cd0798903c
Hope Nkosi <nomthandazonkosi95@gmail.com>
2023-03-15 19:33:02 +0100

    I did the backend of the application. Coded everything, fixed the errors and made the code much more simpler. Replaced complex code with easier one

---
+++ app/src/main/java/com/example/wordguessinggame/StartGame.java
@@ -0,0 +1,18 @@
+
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+
+
